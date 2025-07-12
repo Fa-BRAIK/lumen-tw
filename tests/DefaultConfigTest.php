9cff4cf7cf0d238510aba7f5720b4ec798877d63
Fa-BRAIK <faroukbraik17@gmail.com>
2025-07-11 20:07:20 -0400

    chore: refactor default config tests

--- tests/DefaultConfigTest.php
+++ tests/DefaultConfigTest.php
@@ -2,17 +2,20 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Arr;
 use Lumen\TwMerge\Support\Contracts\Config;
 
 it('can get the default config', function (): void {
     $defaultConfig = app('twMerge')->getDefaultConfig();
 
-    expect($defaultConfig)->toBeInstanceOf(Config::class)
-        ->and($defaultConfig->cacheSize)->toBe(config('lumen-tw.cache_size'))
-        ->and($defaultConfig->prefix)->toBe(config('lumen-tw.prefix'))
-        ->and($defaultConfig->nonExisting ?? null)->tobeNull()
-        ->and(Arr::get($defaultConfig->classGroups, 'display.0'))->toBe('block')
-        ->and(Arr::get($defaultConfig->classGroups, 'overflow.0.overflow.0'))->toBe('auto')
-        ->and(Arr::get($defaultConfig->classGroups, 'overflow.0.nonExistent'))->toBeNull();
+    expect($defaultConfig)
+        ->toBeInstanceOf(Config::class)
+        ->toHaveProperty('cacheSize', config('lumen-tw.cache_size'))
+        ->toHaveProperty('prefix', config('lumen-tw.prefix'))
+        ->not()
+            ->toHaveProperty('nonExistent')
+        ->and($defaultConfig->classGroups)
+            ->toHaveKey('display.0', 'block')
+            ->toHaveKey('overflow.0.overflow.0', 'auto')
+            ->not()
+                ->toHaveKey('overflow.0.nonExistent');
 });
